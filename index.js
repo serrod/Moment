1b6ec37879ca736cc57e8abaf53f42f576bd24ca
serrod <se.rod928@gmail.com>
2023-07-12 21:08:15 -0400

    q4 solved

--- index.js
+++ index.js
@@ -21,6 +21,18 @@ function getLastMondayOfYear(year)
 {
     let time = moment().year(year).endOf('year');
     time = time.startOf('isoWeek');
-    return time.format("MM/DD/YYYY")
+    return time.format("MM/DD/YYYY");
 }
+
+function differencesBetweenTwoDatesAndTime(date1, time1, date2, time2)
+{ 
+    let m1 = moment(date1+" "+time1,"MM/DD/YYYY HH:mm",true);
+    let m2 = moment(date2+" "+time2,"MM/DD/YYYY HH:mm",true);
+
+    let d = moment.duration(Math.abs(m1.diff(m2)));
+
+    return d.years()+" Years, "+d.months()+" Months, "+d.days()+" Days, "+ d.hours()+" Hours, and "+d.minutes()+" Minutes";
+}
+
+console.log(differencesBetweenTwoDatesAndTime("03/01/2022","13:03","03/01/2022","15:04"));
 
