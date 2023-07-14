7160a6197ae8f006ddea69eb96707fe3a60c391a
serrod <se.rod928@gmail.com>
2023-07-13 21:12:54 -0400

    q6 and q7 solved q5 partial

--- index.js
+++ index.js
@@ -31,8 +31,55 @@ function differencesBetweenTwoDatesAndTime(date1, time1, date2, time2)
 
     let d = moment.duration(Math.abs(m1.diff(m2)));
 
-    return d.years()+" Years, "+d.months()+" Months, "+d.days()+" Days, "+ d.hours()+" Hours, and "+d.minutes()+" Minutes";
+    return d.years()+" Years, "+d.months()+" Months, "+d.days()+" Days, "+d.hours()+" Hours, and "+d.minutes()+" Minutes";
 }
 
-console.log(differencesBetweenTwoDatesAndTime("03/01/2022","13:03","03/01/2022","15:04"));
 
+function closestToNow()
+{
+    //random date range from 01/01/0001 to 01/01/3000 
+    let max = 1095364;
+    let min = 0;
+
+    let num = Math.floor(Math.random() * (max - min + 1)) + min; 
+    
+    let m1 = moment().year(1).startOf('year');
+    let m2 = moment().year(1).startOf('year');
+
+    m1 = m1.add(num,'d');
+    num = Math.floor(Math.random() * (max - min + 1)) + min;
+    m2 = m2.add(num,'d');
+    console.log(m1.format("MM/DD/YYYY"));
+    console.log(m2.format("MM/DD/YYYY"))
+
+
+    return "test";
+}
+
+function countDownMiami()
+{
+    let time = moment().utc().year(2026).startOf('year').add(5,'h');
+    let now = moment()
+    let d = moment.duration(time.diff(now));
+    return (d.months()+(d.years()*12))+" Months, "+d.days()+" Days, "+d.hours()+
+           " Hours, "+d.minutes()+" Minutes, "+ d.seconds()+ " and Seconds";
+}
+
+function countDownQatar()
+{
+    let time = moment().utc().year(2026).startOf('year').subtract(3,'h');
+    let now = moment()
+    let d = moment.duration(time.diff(now));
+    return (d.months()+(d.years()*12))+" Months, "+d.days()+" Days, "+d.hours()+
+           " Hours, "+d.minutes()+" Minutes, "+ d.seconds()+ " and Seconds";
+}
+
+function timezoneHourDifference(date, tz1, tz2)
+{
+    return "test";
+}
+
+//console.log(differencesBetweenTwoDatesAndTime("03/01/2022","13:03","03/01/2022","15:04"));
+//console.log(countDownMiami());
+//console.log(countDownQatar());
+closestToNow();
