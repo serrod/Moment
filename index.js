96530c7508d03feee74056ddb2ed4748e52364b4
serrod <se.rod928@gmail.com>
2023-07-17 16:25:15 -0400

    final solutions

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import moment from "moment";
-
+import moment from "moment-timezone";
 
 function formatAnyInputToStandardDate(input)
 {
@@ -8,12 +7,11 @@ function formatAnyInputToStandardDate(input)
     return moment(input,f,true).format("MM/DD/YYYY");
 }
 
-//const t = ["13/02/2022","03/04/2022","Q3 of 2021", "Tue, 22 Feb 2022"];
-
 function getFirstMondayOfYear(year)
 {
     let time = moment().year(year).startOf('year');
     time = time.add(6,'d').startOf('isoWeek');
+
     return time.format("MM/DD/YYYY");
 }
 
@@ -21,6 +19,7 @@ function getLastMondayOfYear(year)
 {
     let time = moment().year(year).endOf('year');
     time = time.startOf('isoWeek');
+
     return time.format("MM/DD/YYYY");
 }
 
@@ -34,7 +33,6 @@ function differencesBetweenTwoDatesAndTime(date1, time1, date2, time2)
     return d.years()+" Years, "+d.months()+" Months, "+d.days()+" Days, "+d.hours()+" Hours, and "+d.minutes()+" Minutes";
 }
 
-
 function closestToNow()
 {
     //random date range from 01/01/0001 to 01/01/3000 
@@ -45,41 +43,74 @@ function closestToNow()
     
     let m1 = moment().year(1).startOf('year');
     let m2 = moment().year(1).startOf('year');
+    let now = moment();
 
     m1 = m1.add(num,'d');
     num = Math.floor(Math.random() * (max - min + 1)) + min;
     m2 = m2.add(num,'d');
-    console.log(m1.format("MM/DD/YYYY"));
-    console.log(m2.format("MM/DD/YYYY"))
 
-
-    return "test";
+    let d1 = moment.duration(Math.abs(m1.diff(now)));
+    let d2 = moment.duration(Math.abs(m2.diff(now)));
+    
+    if (d1.asSeconds() < d2.asSeconds())
+        return m1.format("MM/DD/YYYY");
+    else
+        return m2.format("MM/DD/YYYY");
 }
 
 function countDownMiami()
 {
-    let time = moment().utc().year(2026).startOf('year').add(5,'h');
+    let time = moment().tz("America/New_York").year(2026).startOf('year');
     let now = moment()
     let d = moment.duration(time.diff(now));
+
     return (d.months()+(d.years()*12))+" Months, "+d.days()+" Days, "+d.hours()+
            " Hours, "+d.minutes()+" Minutes, "+ d.seconds()+ " and Seconds";
 }
 
 function countDownQatar()
 {
-    let time = moment().utc().year(2026).startOf('year').subtract(3,'h');
+    let time = moment().tz("Asia/Qatar").year(2026).startOf('year');
     let now = moment()
     let d = moment.duration(time.diff(now));
+
     return (d.months()+(d.years()*12))+" Months, "+d.days()+" Days, "+d.hours()+
            " Hours, "+d.minutes()+" Minutes, "+ d.seconds()+ " and Seconds";
 }
 
 function timezoneHourDifference(date, tz1, tz2)
 {
-    return "test";
+    let m1 = moment.tz(date,"MM/DD/YYYY hh:mma", tz1).utcOffset();
+    let m2 = moment.tz(date, "MM/DD/YYYY hh:mma", tz2).utcOffset();
+
+    return Math.abs((m1 - m2) / 60)
 }
 
-//console.log(differencesBetweenTwoDatesAndTime("03/01/2022","13:03","03/01/2022","15:04"));
-//console.log(countDownMiami());
-//console.log(countDownQatar());
-closestToNow();
+function getAllSpecificDays(year, month, dayOfWeek)
+{
+    let m = moment().year(year).month(month-1).startOf('month').day(dayOfWeek);
+    let l = [];
+
+    if (m.date() > 7)
+        m.add(7, 'd');
+
+    while(m.month() === month - 1)
+    {
+        l.push(m.format("MM/DD/YYYY"));
+        m.add(7, 'd');
+    }
+
+    return l;
+}
+
+function getWeekOfYear(date)
+{
+    let day = date.slice(0,2);
+    let day2 = parseInt(day);
+
+    day2 = ((day2+9)%12)+1;
+    date = date.replace(day,day2.toString());
+    
+    let m  = moment(date,"MM/DD/YYYY");
+    return(m.week());
+}
